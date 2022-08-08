30882c960419f03724a47aa8096b9798f1a49ebd
Lesley Thuto Johnson <whosekid@gmail.com>
2022-08-08 21:33:36 +0200

    Simple POST endpoint

--- app/app.py
+++ app/app.py
@@ -7,19 +7,21 @@ import data_access.loanees_access as la
 
 app = Flask(__name__)
 
-@app.route('/loanee')
+@app.route('/loanee', methods=['GET', 'POST'])
 def getLoanees():
-	loanees_dic = la.getLoaneesDic()
-	response = app.response_class(
-		response = json.dumps(loanees_dic),
-		status=200,
-		mimetype='application/json'
-	)
-	return response
+	if request.method == 'GET':
+		loanees_dic = la.getLoaneesDic()
+		response = app.response_class(
+			response = json.dumps(loanees_dic),
+			status=200,
+			mimetype='application/json'
+		)
+		return response
 
-@app.route('/loanee')
-def postLoanee():
-	pass
+	if request.method == 'POST':
+		req_body = request.get_data()
+		will_repay = la.willRepayLoan(req_body)
+		return will_repay
 
 @app.route('/')
 def index():
